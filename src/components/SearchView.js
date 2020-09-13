e4984adeede6e3f86f3f7fda7b25da22f5ba4fd0
Tore Haglund <ytterdorr@gmail.com>
2020-09-13 03:16:51 +0200

    Add placeholder to search field

--- src/components/SearchView.js
+++ src/components/SearchView.js
@@ -1,7 +1,5 @@
 import React from "react";
-// import "../css/main.css";
 import "../css/searchView.css";
-// import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/solid.min.css";
 
 const SearchView = (props) => {
   const { searchType } = props.location.state;
@@ -9,7 +7,12 @@ const SearchView = (props) => {
     <div className="search-container">
       <div className="description">{`SEARCH BY ${searchType.toUpperCase()}`}</div>
       {/* <label htmlFor="search-field">Search</label> */}
-      <input type="text" name="search-field" id="search-field"></input>
+      <input
+        type="text"
+        name="search-field"
+        id="search-field"
+        Placeholder={`Enter a ${searchType}`}
+      ></input>
       <button type="button" id="search-button">
         <i class="fas fa-search"></i>
       </button>
