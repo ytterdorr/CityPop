89c0d77c31ed99d70d24fbef9c6bdd76d6574629
Tore Haglund <ytterdorr@gmail.com>
2020-09-13 02:57:03 +0200

    Simple Search View layout
    Does not really handle different display sizes

--- src/components/SearchView.js
+++ src/components/SearchView.js
@@ -1,9 +1,20 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+// import "../css/main.css";
+import "../css/searchView.css";
+// import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/solid.min.css";
 
 const SearchView = (props) => {
   const { searchType } = props.location.state;
-  return <div>Search by {searchType}</div>;
+  return (
+    <div className="search-container">
+      <div className="description">{`SEARCH BY ${searchType.toUpperCase()}`}</div>
+      {/* <label htmlFor="search-field">Search</label> */}
+      <input type="text" name="search-field" id="search-field"></input>
+      <button type="button" id="search-button">
+        <i class="fas fa-search"></i>
+      </button>
+    </div>
+  );
 };
 
 export default SearchView;
