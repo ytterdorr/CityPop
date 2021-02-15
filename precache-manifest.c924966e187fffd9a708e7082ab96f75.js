c0d46a4f4d270a45bdf5db616da375e1017d8df7
Tore <ytterdorr@gmail.com>
2021-02-15 02:00:59 +0100

    Updates

---
+++ precache-manifest.c924966e187fffd9a708e7082ab96f75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcc676a5fe8c58e64e9fffd7c7c4cf3e",
+    "url": "/CityPop/index.html"
+  },
+  {
+    "revision": "680cce28626065e5b3c3",
+    "url": "/CityPop/static/css/main.40b21f2d.chunk.css"
+  },
+  {
+    "revision": "5492664409dc3a234657",
+    "url": "/CityPop/static/js/2.ba3e41bf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CityPop/static/js/2.ba3e41bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "680cce28626065e5b3c3",
+    "url": "/CityPop/static/js/main.a0ee85e6.chunk.js"
+  },
+  {
+    "revision": "881c997567230b64681e",
+    "url": "/CityPop/static/js/runtime-main.98072cd7.js"
+  }
+]);
